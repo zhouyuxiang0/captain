41c177d6840cc8e4ec2321db3f5bbff7cd8ef5dc
zhouyuxiang@epaynfc.com <zhouyuxiang@epaynfc.com>
2023-03-22 17:23:16 +0800

    chore

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, io};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{self, BufReader},
+};
 
 use actix_files::Files;
 use actix_web::{
@@ -6,43 +10,77 @@ use actix_web::{
 };
 use actix_web_lab::respond::Html;
 use rust_embed::RustEmbed;
+use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{certs, pkcs8_private_keys};
 use tera::Tera;
 
 #[derive(RustEmbed)]
-#[folder = "templates/"]
-struct Templates;
+#[folder = "public/"]
+struct Public;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     tracing_subscriber::fmt::init();
+    let config = load_rustls_config();
     // tera
     HttpServer::new(|| {
-        let mut tera = Tera::new("./templates/*").unwrap();
-        tera.add_raw_template("index.html", handle_embedded_file("index.html").as_str())
-            .unwrap();
         App::new()
             // .service(Files::new)
-            .app_data(web::Data::new(tera))
             .wrap(middleware::Logger::default())
             .service(hello)
     })
-    .bind(("127.0.0.1", 8000))?
+    .bind_rustls("127.0.0.1:8000", config)?
     .run()
     .await
 }
 
-#[get("/")]
-async fn hello(
-    tmpl: web::Data<tera::Tera>,
-    query: web::Query<HashMap<String, String>>,
-) -> Result<impl Responder, Error> {
-    let mut context = tera::Context::default();
-    context.insert("test", "val");
-    let s = tmpl
-        .render("index.html", &context)
-        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
-    Ok(Html(s))
+#[get("/{_:.*}")]
+async fn hello(path: web::Path<String>) -> Result<impl Responder, Error> {
+    Ok(Html(handle_embedded_file(path.as_str())))
+}
+
+fn load_rustls_config() -> rustls::ServerConfig {
+    // init server config builder with safe defaults
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth();
+
+    // load TLS key/cert files
+    let cert_file = &mut BufReader::new(File::open("cert.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("key.pem").unwrap());
+
+    // convert files to key/cert objects
+    let cert_chain = certs(cert_file)
+        .unwrap()
+        .into_iter()
+        .map(Certificate)
+        .collect();
+    let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file)
+        .unwrap()
+        .into_iter()
+        .map(PrivateKey)
+        .collect();
+
+    // exit if no keys could be parsed
+    if keys.is_empty() {
+        eprintln!("Could not locate PKCS 8 private keys.");
+        std::process::exit(1);
+    }
+
+    config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
+}
+
+fn handle_embedded_file(path: &str) -> String {
+    let html = Public::get(path).unwrap();
+    let b = std::str::from_utf8(&html.data.as_ref()).unwrap();
+    String::from(b)
+    // match Templates::get(path) {
+    //     Some(content) => HttpResponse::Ok()
+    //         .content_type(from_path(path).first_or_octet_stream().as_ref())
+    //         .body(content.data.into_owned()),
+    //     None => HttpResponse::NotFound().body("404 Not Found"),
+    // }
 }
 
 #[cfg(test)]
@@ -59,16 +97,4 @@ mod tests {
         let resp = test::call_service(&app, req).await;
         assert!(resp.status().is_success())
     }
-}
-
-fn handle_embedded_file(path: &str) -> String {
-    let html = Templates::get(path).unwrap();
-    let b = std::str::from_utf8(html.data.as_ref()).unwrap();
-    String::from(b)
-    // match Templates::get(path) {
-    //     Some(content) => HttpResponse::Ok()
-    //         .content_type(from_path(path).first_or_octet_stream().as_ref())
-    //         .body(content.data.into_owned()),
-    //     None => HttpResponse::NotFound().body("404 Not Found"),
-    // }
 }
